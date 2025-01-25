2989e7059a25ebd0724313028c05991427fa9123
Leo Ameb <leoameb@gmail.com>
2025-01-25 13:07:16 -0600

    base.admin: add BaseAdmin and custom AdminSite for Amza Admin interface

---
+++ src/base/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+
+class BaseAdmin(admin.ModelAdmin):
+    timestamps = ('created_at', 'updated_at')
+    
+    def get_list_display(self, request):
+        return self.list_display + self.timestamps
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Hook for specifying custom readonly fields.
+        """
+        return self.readonly_fields + self.timestamps
+
+
+class AdminSite(admin.AdminSite):
+    site_header = 'Amza Admin'
+    site_title = 'Amza Admin Portal'
+    index_title = 'Welcome to Amza Admin Portal'
+
+admin_site = AdminSite()
