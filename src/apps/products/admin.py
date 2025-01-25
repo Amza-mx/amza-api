cdd38a532d15d242f7074fcfd9c55b9ab7228f8a
Leo Ameb <leoameb@gmail.com>
2025-01-25 13:14:17 -0600

    feat: enhance admin interface for Product and ProductPrice models with improved display and search options

--- src/apps/products/admin.py
+++ src/apps/products/admin.py
@@ -1,8 +1,19 @@
-from django.contrib import admin
-from apps.products.models import Product
+from django.contrib.admin import register
+from base.admin import BaseAdmin
+from apps.products.models import Product, ProductPrice
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['sku', 'product_title']
-    search_fields = ['sku', 'product_title']
+@register(Product)
+class ProductAdmin(BaseAdmin):
+    list_display = ('title', 'sku', 'external_id',)
+    search_fields = ('title', 'sku', 'external_id',)
+
+
+@register(ProductPrice)
+class ProductPriceAdmin(BaseAdmin):
+    list_display = ('product', 'amount')
+    search_fields = ('product__sku', 'product_external_id',)
+    raw_id_fields = ('product',)
+
+    def get_product_title(self, obj):
+        return obj.product.title
