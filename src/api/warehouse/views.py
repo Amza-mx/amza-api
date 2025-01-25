7ca78c3fc672f90fb54addd0377e1a6e0be47c96
Leo Ameb <leoameb@gmail.com>
2025-01-25 13:10:15 -0600

    refactor: update Warehouse model import to use prep_centers instead of warehouse

--- src/api/warehouse/views.py
+++ src/api/warehouse/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from apps.warehouse.models import Warehouse
+from apps.prep_centers.models import Warehouse
 from api.warehouse.serializers import WarehouseSerializer
 
 class WarehouseViewSet(viewsets.ModelViewSet):
