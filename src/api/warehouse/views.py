14fa10c7ded6f102ee8f6b80ac88eb3efc2b472e
Leo Ameb <leoameb@gmail.com>
2025-02-01 11:29:56 -0600

    refactor: remove Warehouse serializer and viewset

--- src/api/warehouse/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from apps.prep_centers.models import Warehouse
-from api.warehouse.serializers import WarehouseSerializer
-
-class WarehouseViewSet(viewsets.ModelViewSet):
-    queryset = Warehouse.objects.all()
-    serializer_class = WarehouseSerializer
