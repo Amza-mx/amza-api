fd5ac74717343d97f11c371bb41fe71cbf06de95
Leo Ameb <leoameb@gmail.com>
2025-02-01 12:34:42 -0600

    feat: integrate JWT authentication with REST framework and add auth URLs

---
+++ src/api/v1/auth/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+	TokenObtainPairView,
+	TokenRefreshView,
+	TokenVerifyView
+)
+
+
+urlpatterns = [
+	path('auth/',
+	  	include([
+			path('jwt/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+			path('jwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),	
+			path('jwt/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+		])
+	)
+]
