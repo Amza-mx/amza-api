012a8a7dc1b2ac7e1b2e25f9dffdb2b6e2e88593
Leonardo Amaya <sisdtic6@gmail.com>
2025-02-01 12:34:42 -0600

    feat: integrate JWT authentication with REST framework and add auth URLs

--- src/amza_api/settings.py
+++ src/amza_api/settings.py
@@ -41,6 +41,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
 	'rest_framework',
+	'rest_framework_simplejwt',
 ]
 
 OWN_APPS = [
@@ -140,9 +141,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-	'DEFAULT_AUTHENTICATION_CLASSES': [
-		'rest_framework_simplejwt.authentication.JWTAuthentication'
-	],
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework_simplejwt.authentication.JWTAuthentication',
+	),
 	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 	'PAGE_SIZE': 100,
 }
--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -4,6 +4,7 @@ paths = [
     # path('', include('apps.sales_orders.urls')),
     # path('', include('apps.products.urls')),
     # path('', include('apps.customers.urls')),
+	path('', include('api.v1.auth.urls')),
 	path('', include('api.v1.prep_centers.urls')),
 ]
 
