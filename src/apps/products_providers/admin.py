54f51ebdbc4aac1936c27bd6cbc899a1c13139ea
Leo Ameb <leoameb@gmail.com>
2025-02-03 10:57:02 -0600

    refactor: remove products_providers app and related models, admin, and config

--- src/apps/products_providers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from apps.products_providers.models import ProductProvider
-
-
-@admin.register(ProductProvider)
-class SellerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'is_active']
-    search_fields = ['name',]
-    list_filter = ['is_active']
