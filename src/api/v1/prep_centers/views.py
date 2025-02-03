4509fd12e3f1ab435dd523d5f09e1ce8497d0b81
Leo Ameb <leoameb@gmail.com>
2025-02-03 11:01:34 -0600

    feat: enforce authentication for PrepCenter API access

--- src/api/v1/prep_centers/views.py
+++ src/api/v1/prep_centers/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from apps.prep_centers.models import PrepCenter
 from api.v1.prep_centers.serializers import PrepCenterSerializer
 
 
 class PrepCenterModelViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAuthenticated]
 	queryset = PrepCenter.objects.all()
 	serializer_class = PrepCenterSerializer
