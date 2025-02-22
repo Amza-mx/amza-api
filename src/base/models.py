e58a65f566b34abc406b12bec50e6d6e382453d5
Leonardo Amaya <jose.amaya@yuhu.mx>
2025-02-22 11:49:56 -0600

    feat: enhance BaseModel documentation with detailed attributes and usage instructions

--- src/base/models.py
+++ src/base/models.py
@@ -2,7 +2,25 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    """Base model for all models in the project"""
+    """
+    BaseModel is an abstract class that provides automatic timestamp fields for model instances.
+    Attributes:
+        created_at (DateTimeField): Automatically set to the current date and time when a record is created.
+        updated_at (DateTimeField): Automatically updated to the current date and time whenever a record is modified.
+    Meta Attributes:
+        abstract (bool): Indicates that this class is abstract and should not create a corresponding database table.
+        ordering (tuple): Specifies the default ordering of objects, sorted by the creation timestamp in descending order.
+
+    How to Implement:
+    To use BaseModel, simply subclass it in your Django model. For example:
+
+        class YourModel(BaseModel):
+            # Add your custom fields
+            name = models.CharField(max_length=255)
+            description = models.TextField()
+
+    This way, YourModel automatically inherits the created_at and updated_at fields and their behaviors.
+    """
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
