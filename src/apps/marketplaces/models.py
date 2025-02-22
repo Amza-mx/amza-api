6ae8889e64a82a1f126785bacf689aba8a07e1a8
Leo Ameb <leoameb@gmail.com>
2025-02-22 12:09:12 -0600

    feat: enhance Marketplace model documentation with detailed attributes and methods

--- src/apps/marketplaces/models.py
+++ src/apps/marketplaces/models.py
@@ -3,6 +3,20 @@ from django.db import models
 
 
 class Marketplace(BaseModel):
+    """
+    Marketplace model representing a marketplace with a unique name and a chosen platform.
+    Attributes:
+        name (str): The name of the marketplace with a maximum length of 50 characters.
+        platform (str): The platform type of the marketplace, controlled by the PlatformChoices enum.
+            It defaults to 'AMAZON' and can be either 'AMAZON' or 'WOOCOMMERCE'.
+    PlatformChoices:
+        A subclass that defines the allowed platform values:
+            - AMAZON: Represents the 'AMAZON' platform, with a display value of 'amazon'.
+            - WOOCOMMERCE: Represents the 'WOOCOMMERCE' platform, with a display value of 'woocommerce'.
+    Methods:
+        __str__():
+            Returns a string representation of the marketplace in the format "name - platform".
+    """
     name = models.CharField(max_length=50)
 
     class PlatformChoices(models.TextChoices):
