d1ef46cf049ebbacccf68894bc0707b66e875546
Leonardo Ameb <leoameb@gmail.com>
2025-03-23 16:23:17 -0600

    refactor: update MarketplaceSerializer to use ModelSerializer for improved serialization

--- src/api/v1/marketplaces/serializers.py
+++ src/api/v1/marketplaces/serializers.py
@@ -1,9 +1,9 @@
+from rest_framework import serializers
 from apps.marketplaces.models import Marketplace
-from ..common.serializers import TimestampedModelSerializer
 
 
-class MarketplaceSerializer(TimestampedModelSerializer):
+class MarketplaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Marketplace
-        fields = ['id', 'name', 'platform', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at'] 
+        fields = ('id', 'name', 'platform', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
