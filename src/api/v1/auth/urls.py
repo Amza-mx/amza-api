6a13dcbde1710f01a79520565e7aafc9415b2d04
Leonardo Ameb <leoameb@gmail.com>
2025-03-23 16:22:43 -0600

    refactor: replace JWT authentication paths with a router for cleaner URL management in auth module

--- src/api/v1/auth/urls.py
+++ src/api/v1/auth/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework.routers import DefaultRouter
+from .views import AuthViewSet
 
+router = DefaultRouter()
+router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = [
-    path(
-        'auth/',
-        include(
-            [
-                path('jwt/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-                path('jwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-                path('jwt/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-            ]
-        ),
-    )
+    path('', include(router.urls)),
 ]
