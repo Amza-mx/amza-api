f888fcbac379d3316158c003531d4855a8df78df
Leonardo Ameb <leoameb@gmail.com>
2025-06-07 10:50:38 -0600

    refactor: enhance SalesOrderViewSet documentation to clarify its purpose and functionality

--- src/api/v1/sales_orders/views.py
+++ src/api/v1/sales_orders/views.py
@@ -4,5 +4,12 @@ from api.v1.sales_orders.serializers import SalesOrderSerializer
 
 
 class SalesOrderViewSet(ModelViewSet):
+    """
+    ViewSet for managing SalesOrder instances.
+
+    This viewset leverages Django REST Framework's ModelViewSet to provide 
+    CRUD operations (create, retrieve, update, and delete) for the SalesOrder model.
+    It uses SalesOrderSerializer for data serialization and deserialization.
+    """
     queryset = SalesOrder.objects.all()
     serializer_class = SalesOrderSerializer
