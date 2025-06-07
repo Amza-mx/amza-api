961b82e7d4f026f821963d87f20feaa9531f56c1
Leonardo Ameb <leoameb@gmail.com>
2025-06-07 10:50:12 -0600

    refactor: enhance MarketplaceViewSet documentation and clarify its purpose for better understanding

--- src/api/v1/marketplaces/views.py
+++ src/api/v1/marketplaces/views.py
@@ -5,8 +5,14 @@ from .serializers import MarketplaceSerializer
 
 class MarketplaceViewSet(viewsets.ModelViewSet):
     """
-    ViewSet for viewing and editing marketplaces.
+    ViewSet for managing Marketplace instances.
+
+    This viewset leverages Django REST Framework's ModelViewSet to provide 
+    CRUD operations (create, retrieve, update, and delete) for the Marketplace model.
+    It uses MarketplaceSerializer for data serialization and deserialization.
+
     """
+    # TODO: Add permission classes
     queryset = Marketplace.objects.all()
     serializer_class = MarketplaceSerializer
     search_fields = ['name', 'platform']
