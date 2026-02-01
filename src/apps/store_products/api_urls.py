db48f7854e765efd1203e2ecaf97ade22b49bfdf
Leo Ameb <leoameb@gmail.com>
2026-02-01 14:56:05 -0600

    api_urls removed

--- src/apps/store_products/api_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from api.v1.webhooks.views import KeepaWebhookView
-
-urlpatterns = [
-    # Redirigir a la nueva vista basada en clases
-    path('webhooks/keepa', KeepaWebhookView.as_view(), name='keepa_webhook'),
-]
